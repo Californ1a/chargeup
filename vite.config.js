0f49057366a66788c654b60e822ca853ac27b9ae
Californ1a <rk_california@yahoo.com>
2022-08-22 00:31:09 -0400

    drop console logs in build

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,18 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    }
-  },
-  base: './',
-  build: {
-    sourcemap: true
-  }
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src',
+				import.meta.url)),
+		}
+	},
+	base: './',
+	build: {
+		sourcemap: true
+	},
+	esbuild: {
+		drop: ['console']
+	}
 })
