ab9286abba864c32728863d282d2d295ff419fa2
Californ1a <rk_california@yahoo.com>
2022-08-27 17:14:56 -0400

    remove 0 padding on timer for smaller times

--- src/components/TheTimer.vue
+++ src/components/TheTimer.vue
@@ -18,12 +18,12 @@ function convertMsToTimer(time) {
 	const hours = Math.floor((time / 1000) / 3600);
 	const minutes = Math.floor(((time / 1000) - (hours * 3600)) / 60);
 	const seconds = (time / 1000) - (hours * 3600) - (minutes * 60);
-	let hrStr = `0${hours}`.slice(-2);
-	let minStr = `0${minutes}`.slice(-2);
+	let hrStr = (hours < 10) ? `${hours}`.slice(-2) : `0${hours}`.slice(-2);
+	let minStr = (minutes < 10 && hours <= 0) ? `${minutes}`.slice(-2) : `0${minutes}`.slice(-2);
 	hrStr = hours > 0 ? `${hrStr}:` : '';
 	minStr = (minutes > 0 || hours > 0) ? `${minStr}:` : '';
 	let [secStr, msStr] = `0${seconds}`.split('.');
-	secStr = `0${secStr}`.slice(-2);
+	secStr = (seconds < 10 && minutes <= 0 && hours <= 0) ? `${secStr}`.slice(-2) : `0${secStr}`.slice(-2);
 	msStr = (msStr) ? msStr.slice(0, 2) : '00';
 
 	const formatted = `${hrStr}${minStr}${secStr}.${msStr}`;
